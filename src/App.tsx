867122ebfb585bab2aaa9c21898aa57e2ff4d7bb
Maksym Rozhanskyi <maksym.rozhanskyi@itomy.ch>
2022-04-14 22:58:07 +0300

    loading centered

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "antd/dist/antd.css";
-import React, { FC } from 'react';
-import './App.css';
-import LayoutWrap from './components/LayoutWrap';
+import React, { FC } from "react";
+import "./App.css";
+import LayoutWrap from "./components/LayoutWrap";
 
-const App:FC = () => {
-  return (
-    <LayoutWrap/>
-  );
-}
+const App: FC = () => {
+  return <LayoutWrap />;
+};
 
 export default App;
